029f00e094bf036b2991781b0435a110f6873416
MiNa SaLib <99540302+MiNa-SaLib@users.noreply.github.com>
2022-08-12 15:35:55 +0200

    Create index.js

--- index.js
+++ index.js
@@ -9,21 +9,23 @@ let Year = date.getFullYear();
 let hours = date.getHours();
 let minuts = date.getMinutes();
 let fulldate;
+console.log(date.toDateString());
+console.log(date.toString());
 //custome date and time
 if (minuts < 10) {
   minuts = `0${minuts}`;
 }
 if (hours > 12 && hours <= 23) {
   hours = hours - 12;
-  fulldate = `${day}/${month}/${Year}   ${hours}:${minuts} PM`;
+  fulldate = `${date.toDateString()}   ${hours}:${minuts} PM`;
 }
 
 if (hours >= 0 && hours <= 12) {
   if (hours == 0) {
     hours = 12;
-    fulldate = `${day}/${month}/${Year}   ${hours}:${minuts} AM`;
+    fulldate = `${date.toDateString()}   ${hours}:${minuts} AM`;
   } else {
-    fulldate = `${day}/${month}/${Year}   ${hours}:${minuts} AM`;
+    fulldate = `${date.toDateString()}   ${hours}:${minuts} AM`;
   }
 }
 
@@ -32,7 +34,9 @@ console.log(hours + ":" + minuts);
 
 // remove all notes
 removeall.addEventListener("click", function () {
-  let confirm = window.confirm("You are about to remove all your notes!");
+  let confirm = window.confirm(
+    "You are about to remove all your notes,Are you sure?"
+  );
   console.log(confirm);
   if (confirm) {
     localStorage.clear();
@@ -40,8 +44,12 @@ removeall.addEventListener("click", function () {
   }
 });
 //set localstorag
-if (window.localStorage.getItem("allnotes")) {
+if (
+  window.localStorage.getItem("allnotes") ||
+  window.localStorage.getItem("color")
+) {
   allnotes.innerHTML = window.localStorage.getItem("allnotes");
+  document.body.style.backgroundColor = window.localStorage.getItem("color");
 }
 //save a note
 savebtn.addEventListener("click", function () {
@@ -79,4 +87,33 @@ document.addEventListener("click", function (e) {
     window.localStorage.getItem("allnotes");
     allnotes.innerHTML = window.localStorage.getItem("allnotes");
   }
+});
+//settings div
+let settingsElementi = document.querySelector(".settings .fa-gear");
+let settingsDiv = document.querySelector(".settings");
+let none = document.querySelector(".num1");
+let black = document.querySelector(".num2");
+let yellow = document.querySelector(".num3");
+//heideen and display menue
+settingsElementi.addEventListener("click", function () {
+  settingsDiv.classList.toggle("marginleftforsettings");
+});
+//add colors to localstorage
+none.addEventListener("click", function () {
+  console.log("num1");
+  window.localStorage.removeItem("color");
+  window.localStorage.getItem("color");
+  document.body.style.backgroundColor = window.localStorage.getItem("color");
+});
+black.addEventListener("click", function () {
+  console.log("num2");
+  window.localStorage.setItem("color", "#000");
+  window.localStorage.getItem("color");
+  document.body.style.backgroundColor = window.localStorage.getItem("color");
+});
+yellow.addEventListener("click", function () {
+  console.log("num3");
+  window.localStorage.setItem("color", "#ffde03");
+  window.localStorage.getItem("color");
+  document.body.style.backgroundColor = window.localStorage.getItem("color");
 });
